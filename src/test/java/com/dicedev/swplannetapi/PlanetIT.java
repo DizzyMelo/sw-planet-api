1f1d1dfc15f2f65ffb97511db3228bbdd41ae3af
Daniel Melo <daniel.melo42@outlook.com>
2023-11-25 14:26:39 -0600

    incluide tests for listing planets

--- src/test/java/com/dicedev/swplannetapi/PlanetIT.java
+++ src/test/java/com/dicedev/swplannetapi/PlanetIT.java
@@ -1,15 +1,91 @@
 package com.dicedev.swplannetapi;
 
+import static com.dicedev.swplannetapi.common.PlanetConstants.ALDERAAN;
+import static com.dicedev.swplannetapi.common.PlanetConstants.PLANET;
+import static com.dicedev.swplannetapi.common.PlanetConstants.TATOOINE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.dicedev.swplannetapi.domain.Planet;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/import_planets.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = { "/remove_planets.sql" }, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
     @Test
-    public void contextLoads() {
+    public void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void getPlanetById_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void getPlanetByName_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/name/" + TATOOINE.getName(), Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void getPlanets_ReturnsAllPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets/", Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).hasSize(3);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void getPlanets_ByClimate_ReturnsPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets/?climate=" + ALDERAAN.getClimate(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).hasSize(2);
+        assertThat(sut.getBody()[0]).isEqualTo(ALDERAAN);
+    }
+
+    @Test
+    public void getPlanets_ByTerrain_ReturnsPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets/?terrain=desert", Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).hasSize(1);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void removePlanet_ReturnsNoContent() {
+        ResponseEntity<Void> sut = restTemplate.exchange("/planets/{id}", HttpMethod.DELETE, null, Void.class, TATOOINE.getId());
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/com/dicedev/swplannetapi/common/PlanetConstants.java
+++ src/test/java/com/dicedev/swplannetapi/common/PlanetConstants.java
@@ -9,9 +9,9 @@ public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
     public static final Planet EMPTY_PLANET = new Planet();
-    public static final Planet TATOOINE = new Planet("Tatooine", "arid", "desert");
-    public static final Planet ALDERAAN = new Planet("Alderaan", "temperate", "grassland, mountains");
-    public static final Planet YAVIN_IV = new Planet("Yavin IV", "temperate", "jungle, rainforest");
+    public static final Planet TATOOINE = new Planet(1, "Tatooine", "arid", "desert");
+    public static final Planet ALDERAAN = new Planet(2, "Alderaan", "temperate", "grassland, mountains");
+    public static final Planet YAVIN_IV = new Planet(3, "Yavin IV", "temperate", "jungle, rainforest");
 
     public static final List<Planet> PLANETS = List.of(TATOOINE, ALDERAAN, YAVIN_IV);
     
