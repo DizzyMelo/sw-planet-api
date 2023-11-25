96aa0fc8fc11986df1f63b8bd061bb2f046015b5
Daniel Melo <daniel.melo42@outlook.com>
2023-11-25 10:40:11 -0600

    mysql database configuration

--- src/test/java/com/dicedev/swplannetapi/PlanetIT.java
+++ src/test/java/com/dicedev/swplannetapi/PlanetIT.java
@@ -3,10 +3,13 @@ package com.dicedev.swplannetapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
-    
+
     @Test
-    public void contextLoads() {}
+    public void contextLoads() {
+    }
 }
